aa950c8aa134a39f5f7a6e4902c30972dbe06a5b
A.A <115159286+Ade-mir@users.noreply.github.com>
2023-03-08 14:27:50 +0100

    Added new repo with proper .ignore settings.

---
+++ components/Header.jsx
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from "react";
+import Link from "next/link";
+import { getCategories } from "../services";
+
+const Header = () => {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    getCategories().then((newCategories) => setCategories(newCategories));
+  }, []);
+
+  return (
+    <div className="container mx-auto px-10 mb-8">
+      <div className=" w-full inline-block py-8">
+        <div className="md:float-left block">
+          <Link href="/">
+            <span className="cursor-pointer font-bold text-4xl">
+              How To Become A Developer Blog
+            </span>
+          </Link>
+        </div>
+        <div className="hidden md:float-left md:contents">
+          {categories.map((category) => (
+            <Link key={category.slug} href={`/category/${category.slug}`}>
+              <span className="md:float-right mt-2 align-middle ml-4 font-semibold cursor-pointer">
+                {category.name}
+              </span>
+            </Link>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
